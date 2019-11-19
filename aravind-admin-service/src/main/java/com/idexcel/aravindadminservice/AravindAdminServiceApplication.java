9c318130f1e5ed46d5408f9cd92cec42807473c1
Aravind <akalimurthy@gmail.com>
2019-11-18 20:16:42 -0500

    Initial commit of admin service API project with all packages and dependencies.

---
+++ aravind-admin-service/src/main/java/com/idexcel/aravindadminservice/AravindAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.idexcel.aravindadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AravindAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AravindAdminServiceApplication.class, args);
+	}
+
+}
---
+++ aravind-admin-service/src/test/java/com/idexcel/aravindadminservice/AravindAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idexcel.aravindadminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AravindAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
